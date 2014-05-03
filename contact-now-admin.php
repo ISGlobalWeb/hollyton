c895f0303a6500abc745ad438a6ac29877eaec41
Chetna Agarwal <chetna@isglobalweb.com>
2014-05-03 11:21:03 +0530

    contact-admin page created

---
+++ contact-now-admin.php
@@ -0,0 +1,92 @@
+<table width="580" align="center" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <th colspan="2" align="center" scope="col"><span style="font-family: century gothic,sans-serif;"><img src="http://benham.com.dedi1544.your-server.de/websites/hollyton/assets/images/logo.png" width="140" height="140" alt="Rentals London Logo" /></span></th>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2"><p><font color="#000000" face="verdana" size="2">Hi ABCD<br /><br />Thank you for your enquiry.<br />
+              <br />
+		Your request is being dealt with and one of our lettings staff will contact you soon.<br /><br />
+        The following enquiry was submitted from Property of the Month form to the website:<br /> <a href="http://www.hollyton.co.uk/" style="color:#c00;" target="_blank">www.rentals-london.co.uk</a>
+	</font></p></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2">
+     <table width="100%" border="0" align="center" cellpadding="5" cellspacing="0" bgcolor="#" style="border: 1px solid #155959; border-top: none; font-family:verdana; font-size:14px;">
+  <tr>
+<td colspan="2" valign="top" bgcolor="#155959" style="color:#fff; font-size:16px;"><strong>Enquiry Details </strong> </td>
+  </tr>
+  <tr>
+    <td width="26%" bgcolor="#FFFFFF">&nbsp;</td>
+    <td width="74%" bgcolor="#FFFFFF">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#f3f2dd">Name :</td>
+    <td bgcolor="#f3f2dd">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FFFFFF">Email :</td>
+    <td bgcolor="#FFFFFF">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#f3f2dd">Phone :</td>
+    <td bgcolor="#f3f2dd">&nbsp;</td>
+  </tr>
+  <tr>
+    <td bgcolor="#FFFFFF">Comments :</td>
+    <td bgcolor="#FFFFFF">&nbsp;</td>
+  </tr>
+    <tr>
+    <td bgcolor="#f3f2dd">&nbsp;</td>
+    <td bgcolor="#f3f2dd">&nbsp;</td>
+  </tr>
+
+</table>
+</td>
+  </tr>
+  
+    <tr>
+    <td colspan="2">
+    <table width="100%" border="0" cellspacing="0" cellpadding="0" style="border-bottom:1px solid #155959;">
+  <tr>
+    <th scope="col">&nbsp;</th>
+  </tr>
+  <tr>
+    <td><font color="#000000" face="verdana" size="2">The above was submitted on the 3-5-2014.
+    <br><br>
+    Kind regards,
+    <br>
+    Rentals London
+		<br />
+		<a href="http://www.rentals-london.co.uk/" style="color:#c00;" target="_blank">www.hollyton.co.uk/</a>
+    </font><br><br>
+    <p style="padding-right:25px;"><font color="#000000" face="verdana" size="1">*Please do not reply to this e-mail. </font></p>
+    </td>
+  </tr>
+</table>
+</td>
+  </tr>
+
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
