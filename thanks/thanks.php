7b4b81ee80d3b2d526436a3f7a6c567052b1d849
Chetna Agarwal <chetna@isglobalweb.com>
2014-05-03 13:11:37 +0530

    update thanks page

---
+++ thanks/thanks.php
@@ -0,0 +1,49 @@
+<?php include_once "../globalpath.php";?>
+<!DOCTYPE HTML>
+<!--[if lt IE 9]><html class="ie"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="canonical" href="<?php echo GLOBAL_PATH;?>/looking-invest/"/>
+<title>Thank you for your enquiry!!</title>
+<meta name="description" content="Welcome to Hollyton Website" />
+<meta name="keywords" content="Hollyton" />
+<?php include "../include/common-files.php";?>
+<!-- main mobile Js -->
+</head>
+<body lang="en">
+<div id="wrapper"><!---->
+<!--Header Start Here-->
+<header>
+<div class="banner">
+<img src="<?php echo GLOBAL_PATH;?>/assets/images/looking-invest-banner.jpg" alt="Looking to Invest Banner" >
+</div>
+<div class="header-outer">
+<div class="header-inner">
+<?php include "../include/header.php";?>
+</div>
+</div> 
+<!--Caption start here-->
+<div class="thanks-content">
+	<div class="thanks-inner">
+<h1>Thank you for your enquiry.</h1>
+  <p class="co-black">Your request is being dealt with and one of our dedicated agents will contact you soon.</p>
+   </div>
+    </div>
+<!--Caption End Here-->
+<div class="clear"></div>
+</header>
+<!--Header End Here-->
+
+<!--Content start Here-->
+
+<!--Content end Here-->
+<!--Footer Start Here-->
+<footer>
+<?php include "../include/footer.php";?>
+</footer>
+<!--Footer End Here-->
+</div>
+
+</body>
+</html>
