f63142131129411c60b594d2783c7eb78d678c39
Chetna Agarwal <chetna@isglobalweb.com>
2014-05-03 14:17:57 +0530

    404 update

---
+++ 404.php
@@ -0,0 +1,60 @@
+<?php include_once "globalpath.php";?>
+<!DOCTYPE HTML>
+<!--[if lt IE 9]><html class="ie"><![endif]-->
+<!--[if gte IE 9]><!--><html><!--<![endif]-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="canonical" href="<?php echo GLOBAL_PATH;?>/404/"/>
+<title>Why London?</title>
+<meta name="description" content="Page not found" />
+<meta name="keywords" content="Hollyton" />
+<?php include "include/common-files.php";?>
+<!-- main mobile Js -->
+</head>
+<body lang="en">
+<div id="wrapper"><!---->
+<!--Header Start Here-->
+<header>
+<div class="banner">
+<img src="<?php echo GLOBAL_PATH;?>/assets/images/why-london-banner.jpg" alt="Why London Banner" >
+</div>
+<div class="header-outer">
+<div class="header-inner">
+<?php include "include/header.php";?>
+</div>
+</div> 
+<!--404 start here-->
+<div class="thanks-content">
+	<div class="thanks-inner">
+        <div class="content-part">
+        <div class="content-left">
+        <h1 class="f-80 co-red">404<br>
+ERROR</h1>
+<p class="co-black">Oops! Unable to show this page at the moment, maybe later... but hey sure we can give you the information you need, you can look for our services or contact us, we are happy to talk to you.</p>
+        </div>
+<div class="content-right">
+  <img src="<?php echo GLOBAL_PATH;?>/assets/images/404.png" alt="404"></div>
+
+        </div>   </div>
+    </div>
+<!--404 End Here-->
+<div class="clear"></div>
+</header>
+<!--Header End Here-->
+
+<!--Content start Here-->
+<figure id="content">
+    <div id="heading">
+        
+    </div>
+</figure>
+<!--Content end Here-->
+<!--Footer Start Here-->
+<footer>
+<?php include "include/footer.php";?>
+</footer>
+<!--Footer End Here-->
+</div>
+
+</body>
+</html>
