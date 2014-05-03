8236dfa859074e37644b1381670f613712f68d2c
Chetna Agarwal <chetna@isglobalweb.com>
2014-05-03 11:26:18 +0530

    contact-now page added

--- contact-now-admin.php
+++ contact-now-admin.php
@@ -1,6 +1,6 @@
 <table width="580" align="center" border="0" cellspacing="0" cellpadding="0">
   <tr>
-    <th colspan="2" align="center" scope="col"><span style="font-family: century gothic,sans-serif;"><img src="http://benham.com.dedi1544.your-server.de/websites/hollyton/assets/images/logo.png" width="140" height="140" alt="Rentals London Logo" /></span></th>
+    <th colspan="2" align="center" scope="col"><span style="font-family: century gothic,sans-serif;"><a href="http://www.hollyton.co.uk/"><img src="http://benham.com.dedi1544.your-server.de/websites/hollyton/assets/images/logo.png" width="140" height="140" alt="Rentals London Logo" /></a></span></th>
   </tr>
   <tr>
     <td>&nbsp;</td>
@@ -10,7 +10,7 @@
     <td colspan="2"><p><font color="#000000" face="verdana" size="2">Hi ABCD<br /><br />Thank you for your enquiry.<br />
               <br />
 		Your request is being dealt with and one of our lettings staff will contact you soon.<br /><br />
-        The following enquiry was submitted from Property of the Month form to the website:<br /> <a href="http://www.hollyton.co.uk/" style="color:#c00;" target="_blank">www.rentals-london.co.uk</a>
+        The following enquiry was submitted from Property of the Month form to the website:<br /> <a href="http://www.hollyton.co.uk/" style="color:#c00;" target="_blank">www.hollyton.co.uk</a>
 	</font></p></td>
   </tr>
   <tr>
@@ -65,7 +65,7 @@
     <br>
     Rentals London
 		<br />
-		<a href="http://www.rentals-london.co.uk/" style="color:#c00;" target="_blank">www.hollyton.co.uk/</a>
+		<a href="http://www.hollyton.co.uk/" style="color:#c00;" target="_blank">www.hollyton.co.uk/</a>
     </font><br><br>
     <p style="padding-right:25px;"><font color="#000000" face="verdana" size="1">*Please do not reply to this e-mail. </font></p>
     </td>
