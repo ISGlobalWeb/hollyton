5c380982fce1ceb5876aabaee9a0498024e11558
siddharth sinha <sid.style027@gmail.com>
2014-05-14 19:08:42 +0530

    404 page upade

--- 404.php
+++ 404.php
@@ -2,54 +2,52 @@
 <!DOCTYPE HTML>
 <!--[if lt IE 9]><html class="ie"><![endif]-->
 <!--[if gte IE 9]><!--><html><!--<![endif]-->
+
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="canonical" href="<?php echo GLOBAL_PATH;?>/404/"/>
-<title>Why London?</title>
+<title>404 Page not found - Hollytons.co.uk</title>
 <meta name="description" content="Page not found" />
 <meta name="keywords" content="Hollyton" />
 <?php include "include/common-files.php";?>
 <!-- main mobile Js -->
 </head>
 <body lang="en">
-<div id="wrapper"><!---->
-<!--Header Start Here-->
-<header>
-<div class="banner">
-<img src="<?php echo GLOBAL_PATH;?>/assets/images/why-london-banner.jpg" alt="Why London Banner" >
-</div>
-<div class="header-outer">
-<div class="header-inner">
-<?php include "include/header.php";?>
-</div>
-</div> 
-<!--404 start here-->
-<div class="thanks-content">
-	<div class="thanks-inner">
-        <div class="content-part">
-        <div class="content-left">
-        <h1 class="f-80 co-red">404<br>
-ERROR</h1>
-<p class="co-black">Oops! Unable to show this page at the moment, maybe later... but hey sure we can give you the information you need, you can look for our services or contact us, we are happy to talk to you.</p>
-<div class="butt-y f-left"><a href="">Home</a></div>
-<div class="butt-y f-left"><a href="">Contact Us</a></div>
-        </div>
-<div class="content-right">
-  <img src="<?php echo GLOBAL_PATH;?>/assets/images/404.png" alt="404"></div>
-
-        </div>   </div>
+<div id="wrapper"><!----> 
+  <!--Header Start Here-->
+  <header>
+    <div class="banner"> <img src="<?php echo GLOBAL_PATH;?>/assets/images/why-london-banner.jpg" alt="Why London Banner" > </div>
+    <div class="header-outer">
+      <div class="header-inner">
+        <?php include "include/header.php";?>
+      </div>
     </div>
-<!--404 End Here-->
-
-</header>
-<div class="clear"></div>
-<!--Header End Here-->
-
-<!--Footer Start Here-->
-<footer>
-<?php include "include/footer.php";?>
-</footer>
-<!--Footer End Here-->
+    <!--404 start here-->
+    <div class="thanks-content">
+      <div class="thanks-inner">
+        <div class="content-part">
+          <div class="content-left">
+            <h1 class="f-80 co-red">404<br>
+              ERROR</h1>
+            <p class="co-black">Oops! Unable to show this page at the moment, maybe later... but hey sure we can give you the information you need, you can look for our services or contact us, we are happy to talk to you.</p>
+            <div class="butt-y f-left"><a href="<?php echo GLOBAL_PATH;?>">Home</a></div>
+            <div class="butt-y f-left"><a href="<?php echo GLOBAL_PATH;?>/contact/">Contact Us</a></div>
+          </div>
+          <div class="content-right"> <img src="<?php echo GLOBAL_PATH;?>/assets/images/404.png" alt="404"></div>
+        </div>
+      </div>
+    </div>
+    <!--404 End Here--> 
+    
+  </header>
+  <div class="clear"></div>
+  <!--Header End Here--> 
+  
+  <!--Footer Start Here-->
+  <footer>
+    <?php include "include/footer.php";?>
+  </footer>
+  <!--Footer End Here--> 
 </div>
 <?php include "include/go-to-top.php";?>
 </body>
